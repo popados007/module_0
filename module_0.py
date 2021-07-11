a30e9e24455928c793ddc8ecf6c68b62fadc442a
popados007 <bezspama@gmail.com>
2021-07-11 16:22:19 +0300

    Create module_0.py
    Программа по угадыванию числа

---
+++ module_0.py
@@ -0,0 +1,22 @@
+import numpy as np
+def game_core(number):
+    count = 1
+    predict = 50 # всегда начинает угадывать с середины диапазона
+    min = 0  # минимальное число диапазона
+    max = 101 # максимальное число диапазона
+    while number != predict:  # если не угадали, делим оставшийся диапазон пополам
+        count += 1
+        if number > predict:
+            min = predict
+            predict = min + ((max - min) // 2)
+        elif number < predict:
+            max = predict
+            predict = min + ((max - min) // 2)
+    return(count) # выход из цикла, если угадали
+
+'''Запускаем функцию 1000 раз'''
+predict_count = 0
+for i in range(1000):
+    number = np.random.randint(1, 101)
+    predict_count += game_core(number)
+print(f"Ваш алгоритм угадывает число в среднем за {int(predict_count / i)} попыток")
